9c54db134c7ea49f5e96df942354f428e33a9992
marinaferb92 <mariinota@gmail.com>
2024-12-02 13:24:23 +0100

    Create setup_nfs_server.sh

---
+++ scripts/setup_nfs_server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Configuramos para mostrar los comandos y finalizar
+set -ex
+
+#Importamos el archivo .env
+
+source .env
+
+#Instalamos el NFS Server
+apt install nfs-kernel-server -y
+
+#Creamos el directorio que vamos a compartir
+mkdir -p /var/www/html
+
+#Modificamos el porpietario y el grupo del directorio
+sudo chown nobody:nogroup /var/www/html
+
+#Copiamos el archivo de configuracion de NFS
+cp ../nfs/exports /etc/exports
+
+#Reiniciamos el servicio NFS
+systemctl restart nfs-kernel-server
