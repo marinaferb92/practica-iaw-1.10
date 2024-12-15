cc9254410ba68969b033fd8060b9690f47fca2d3
marinaferb92 <mariinota@gmail.com>
2024-12-15 12:30:32 +0100

    Update wp-config.php

--- conf/wp-config.php
+++ conf/wp-config.php
@@ -1,8 +1,12 @@
+
 <?php
 // Configuración de base de datos
-define('DB_NAME', 'BDmfb');
-define('DB_USER', 'mfb92');
-define('DB_PASSWORD', '1234');
-define('DB_HOST', 'localhost');
+define('DB_NAME', 'wordpress_db')
+define('DB_USER', 'user');
+define('DB_PASSWORD', 'user');
+define('DB_HOST', '172.31.19.88');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
+
+
+?>
